5f641c44933a2dedbb4e043d7f6aca1e820a1615
Zhifa Qiu <zhifaq@Zhifas-Air.bbrouter>
2023-09-05 18:43:20 +1000

    second commit

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const note = process.argv[2];
-const newNote = {
-    id: new Date(),
-    content: note
-};
-console.log(newNote);
